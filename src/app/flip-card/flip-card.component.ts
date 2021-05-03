0f4ae15f50bddcd9ec6cd0f9db4648c63a903e4c
Herman Cheung <hermanc.dev@gmail.com>
2021-05-04 00:54:58 +0800

    video background

---
+++ src/app/flip-card/flip-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'flip-card',
+  templateUrl: './flip-card.component.html',
+  styleUrls: ['./flip-card.component.css']
+})
+export class FlipCardComponent implements OnInit {
+  toggleProperty = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggle() {
+    this.toggleProperty = !this.toggleProperty;
+  }
+
+}
