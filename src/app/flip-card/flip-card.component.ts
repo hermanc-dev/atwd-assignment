c0cd914bff0aa8e39a1290f81168346e08e88949
Herman Cheung <hermanc.dev@gmail.com>
2021-05-30 07:39:12 +0800

    delete dialog

--- src/app/flip-card/flip-card.component.ts
+++ src/app/flip-card/flip-card.component.ts
@@ -188,7 +188,7 @@ export class FlipCardComponent implements OnInit {
   deleteDialog(ROUTE_ID:string){
       const dialogRef=this.dialog.open(ConfirmDialogComponent,{data:{
       routeID:ROUTE_ID,
-    },height: '200px',
+    },height: '160px',
     width: '300px',
     panelClass: 'custom-dialog-container',
   });
